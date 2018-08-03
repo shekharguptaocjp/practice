507ac6f0a15bea8e1a4915d9bf8d53655cfc22af
M1046458 <M1046458@A2ML27492.mindtree.com>
2018-08-03 11:30:17 +0530

    commit

---
+++ employee_management/src/main/resources/data.sql
@@ -0,0 +1,1 @@
+insert into employeedetails (emp_id_n,username_v,password_v,full_name_v,email_id_v,date_of_birthday_v,gender_v,security_question_v,security_answer_v) values (1046458,'ShekharG','abcd','Shekhar Gupta','shekhar.gupta@mindtree.com','06/04/1992','Male','Birth Place','Bhopal');
