863c7417c20f9ae674df70a55ecc88f197387b90
M1046458 <M1046458@A2ML27492.mindtree.com>
2018-08-03 12:37:21 +0530

    nmm

--- employee_management/src/main/resources/schema.sql
+++ employee_management/src/main/resources/schema.sql
@@ -1,15 +1,15 @@
 --drop table employeedetails;
---delete from employeedetails;
-CREATE TABLE employeedetails (
- emp_id_n INT NOT NULL,
-  username_v VARCHAR(45) NOT NULL,
-  password_v VARCHAR(45) NOT NULL,
-  full_name_v VARCHAR(45) NULL,
-  email_id_v VARCHAR(45) NULL,
-  date_of_birthday_v VARCHAR(45) NULL,
-  gender_v VARCHAR(45) NULL,
-  security_question_v VARCHAR(45) NULL,
-  security_answer_v VARCHAR(45) NULL,
-  PRIMARY KEY (emp_id_n));
+
+--CREATE TABLE employeedetails (
+ --emp_id_n INT NOT NULL,
+  --username_v VARCHAR(45) NOT NULL,
+  --password_v VARCHAR(45) NOT NULL,
+  --full_name_v VARCHAR(45) NULL,
+  --email_id_v VARCHAR(45) NULL,
+  --date_of_birthday_v VARCHAR(45) NULL,
+  --gender_v VARCHAR(45) NULL,
+  --security_question_v VARCHAR(45) NULL,
+  --security_answer_v VARCHAR(45) NULL,
+  --PRIMARY KEY (emp_id_n));
   
-  --drop table employeedetails;
+ 
